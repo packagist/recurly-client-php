2dd51e02c9a9e739ff96a8b4bc20cee3c132206f
Stephan <glaubinix@users.noreply.github.com>
2024-05-09 19:49:55 +0100

    Update SubscriptionAddOn_Test.php
    Co-authored-by: Douglas Miller <doug@douglas-miller.com>

--- Tests/Recurly/SubscriptionAddOn_Test.php
+++ Tests/Recurly/SubscriptionAddOn_Test.php
@@ -5,6 +5,6 @@ class Recurly_SubscriptionAddOn_Test extends Recurly_TestCase
     public function testToString() {
         $addon = new \Recurly_SubscriptionAddOn();
 
-        $this->assertSame('<Recurly_SubscriptionAddOn >', $addon->__toString());
+        $this->assertSame('<Recurly_SubscriptionAddOn[new] >', $addon->__toString());
     }
 }
